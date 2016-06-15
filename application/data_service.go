aa29f732ecbb7d4a978089ddba1679ace6b20f9b
dohantaur <dohant.aurelien@gmail.com>
2016-06-15 14:02:53 +0200

    change algo

--- application/data_service.go
+++ application/data_service.go
@@ -14,12 +14,11 @@ type lastData struct {
 }
 
 func (ld *lastData) Add(d bson.M) {
-	ld.m.Lock()
+
 	ld.data = append(ld.data, d)
 	if len(ld.data) >= MAX_DATA {
-		ld.data = ld.data[:1]
+		ld.data = ld.data[1:]
 	}
-	ld.m.Unlock()
 }
 
 var ld *lastData
@@ -29,21 +28,28 @@ func ComputeData(data bson.M) bson.M {
 	hum := 0.0
 	lum := 0.0
 	moi := 0.0
+	ld.m.Lock()
+
 	ld.Add(data)
-	//ld.m.Lock()
 
 	for _, d := range ld.data {
 		fmt.Println(d)
 		if len(d) > 0 {
-			temp = temp + d["t"].(float64)
-			hum = hum + d["h"].(float64)
-			lum = lum + d["l"].(float64)
+			if d["t"] != nil {
+				temp = temp + d["t"].(float64)
+			}
+			if d["h"] != nil {
+				hum = hum + d["h"].(float64)
+			}
+			if d["l"] != nil {
+				lum = lum + d["l"].(float64)
+			}
 			if d["m"] != nil {
 				moi = moi + d["m"].(float64)
 			}
 		}
 	}
-	//ld.m.Unlock()
+	ld.m.Unlock()
 
 	if len(ld.data) > 0 {
 		length := float64(len(ld.data))
